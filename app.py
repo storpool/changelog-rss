3039ea2698a60f4e3d7f7fa79027f380114cc162
Kaloyan Kotlarski <kaloyan.kotlarski@storpool.com>
2022-02-23 17:49:28 +0200

    Add code that extracts the changelog contents from a gerrit change

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import urllib.parse
 
 from flask import Flask, abort, redirect, url_for
 from pygerrit2 import GerritRestAPI, HTTPBasicAuth
@@ -9,6 +10,33 @@ app = Flask(__name__)
 fg = FeedGenerator()
 
 
+def cleanup_markdown(change: str) -> str:
+    return change.replace('`', '')
+
+
+def create_changelog_object(gerrit_client: GerritRestAPI, gerrit_change: dict) -> dict:
+    revision_id = list(gerrit_change["revisions"].keys())[0]
+    file_name = list(gerrit_change["revisions"][revision_id]["files"].keys())[0]
+    diff = gerrit_client.get("/changes/" + gerrit_change["id"] + "/revisions/" + revision_id + "/files/" + urllib.parse.quote_plus(file_name) + "/diff")
+    change_lines = list(filter(lambda line: len(line), diff["content"][1]["b"]))
+
+    changelog = {
+        "revision": change_lines[1].split(" ")[2],
+        "release_date": change_lines[3],
+        "public_changes": [],
+        "private_changes": []
+    }
+
+    private_changes_delimiter_line_index = [change_lines.index(line) for line in change_lines if ".. revision" in line][0]
+    public_lines_slice = change_lines[4:private_changes_delimiter_line_index]
+    private_lines_slice = change_lines[private_changes_delimiter_line_index+1:]
+
+    for line in public_lines_slice:
+        changelog["public_changes"].append(cleanup_markdown(line))
+
+    return changelog
+
+
 def build_feed():
     try:
         gerrit_auth = HTTPBasicAuth(os.environ.get('GERRIT_USERNAME'), os.environ.get('GERRIT_USERNAME'))
@@ -16,8 +44,9 @@ def build_feed():
     except KeyError as e:
         sys.exit(1)
 
-    changes = gerrit_client.get("changes/?q=changelog-19.01:+and+status:merged")
+    changes = gerrit_client.get("changes/?q=changelog-19:+and+status:merged&o=CURRENT_REVISION&o=CURRENT_FILES")
     for change in changes:
+        changelog = create_changelog_object(gerrit_client, change)
         feed_entry = fg.add_entry()
 
 
